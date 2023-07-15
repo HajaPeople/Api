cfede006462c7f3f779b8f4f386b17d5d560a54e
kwondaehwi <kwondh8561@ajou.ac.kr>
2023-07-15 22:18:04 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/hajapeople/haja/HajaApplication.java
@@ -0,0 +1,13 @@
+package com.hajapeople.haja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HajaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HajaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hajapeople/haja/HajaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hajapeople.haja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HajaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
